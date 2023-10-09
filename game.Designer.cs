10a1ec361de381cb44cdb509121fbecab22bcb16
Snow0825 <143132967+Snow0825@users.noreply.github.com>
2023-10-10 03:46:46 +0900

    Add files via upload

---
+++ game.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowsFormsApp11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(984, 620);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 5;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(806, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "score";
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 620);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ game.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        private double x = 0.0;
+        private List<string> keying = new List<string>();
+        private double ex, ey = 0.0;
+
+        Random randomizer = new Random();
+        private int score = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            label1.Text = score.ToString();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //this.Text = ProductName;// ピクチャボックスの設定
+            //pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // 縦横比を変えずに引き延ばす
+            //pictureBox1.Image = Properties.Resources.丹羽安二郎;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillEllipse(Brushes.Red, (int)ex, (int)ey, 10, 10);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // 縦横比を変えずに引き延ばす
+            pictureBox1.Image = Properties.Resources.丹羽安二郎; //e.Graphics.FillRectangle(Brushes.Black, (int)x, 300, 50, 50);
+
+            if ((ex + 10.0 > x && ex + 10.0 < x + 50) || (ex > x && ex < x + 50))
+            {
+                if ((ey + 10.0 > 300 && ey + 10.0 < 300 + 50) || (ey > 300 && ey < 300 + 50))
+                {
+                    timer1.Enabled = false;
+                    timer2.Enabled = false;
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (keying.Contains("Left") && x > 0)
+                x -= 2.0;
+            if (keying.Contains("Right") && x + 50 < pictureBox1.Width)
+                x += 2.0;
+            pictureBox1.Invalidate();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!keying.Contains(e.KeyCode.ToString()))
+                keying.Add(e.KeyCode.ToString());
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (ey >= pictureBox1.Height)
+                ey = 0.0;
+            if (ey == 0.0)
+                ex = randomizer.Next(pictureBox1.Width);
+            else
+                ex += randomizer.Next(5) - 2;
+            ey += 2.0;
+            pictureBox1.Invalidate();
+        }
+
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            keying.Remove(e.KeyCode.ToString());
+        }
+    }
+}
